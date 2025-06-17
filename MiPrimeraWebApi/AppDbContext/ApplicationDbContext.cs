d386358eb9557ff0438df27f5a28b8e0c6b45773
C3sarAcosta <c3sar.adan@gmail.com>
2025-06-17 11:22:23 -0600

    17/06/2025 pt1

---
+++ MiPrimeraWebApi/AppDbContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiPrimeraWebApi.Models;
+
+namespace MiPrimeraWebApi.AppDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
